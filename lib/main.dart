30e29196ec385e8c48a95fd93fb4aad41bf7ae4a
Leman Fotera <lemanfoetra@gmail.com>
2020-05-11 08:38:10 +0700

    Navigation Product -> product detail

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shop/screens/product_detail_screen.dart';
 import './screens/product_overview_screen.dart';
 
 void main() => runApp(MyApp());
@@ -10,11 +11,13 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'MyShop',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.teal,
+        fontFamily: 'Lato',
       ),
       initialRoute: '/',
       routes: {
         '/': (ctx) => ProductOverviewScreeen(),
+        ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
       },
       onUnknownRoute: (setting) {
         return MaterialPageRoute(
--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shop/screens/product_detail_screen.dart';
 
 class ProductItem extends StatelessWidget {
   final String id;
@@ -11,37 +12,44 @@ class ProductItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return InkWell(
       onTap: () {
-        print('print product');
+        Navigator.of(context).pushNamed(
+          ProductDetailScreen.routeName,
+          arguments: id,
+        );
       },
-      child: GridTile(
-        child: Container(
-          decoration: BoxDecoration(
-            border: Border.all(
-              color: Colors.grey,
-              width: 0.3,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(5),
+        child: GridTile(
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(5),
+              border: Border.all(
+                color: Colors.grey,
+                width: 0.3,
+              ),
+            ),
+            child: Image.network(
+              imgUrl,
+              fit: BoxFit.cover,
             ),
           ),
-          child: Image.network(
-            imgUrl,
-            fit: BoxFit.cover,
-          ),
-        ),
-        header: GridTileBar(
-          leading: IconButton(
-            icon: Icon(
-              Icons.favorite_border,
-              color: Colors.red,
+          header: GridTileBar(
+            leading: IconButton(
+              icon: Icon(
+                Icons.favorite_border,
+                color: Colors.red,
+              ),
+              onPressed: () {
+                print('icon favorite');
+              },
             ),
-            onPressed: () {
-              print('icon favorite');
-            },
           ),
-        ),
-        footer: GridTileBar(
-          backgroundColor: Colors.black26,
-          title: Text(
-            title,
-            textAlign: TextAlign.center,
+          footer: GridTileBar(
+            backgroundColor: Colors.black26,
+            title: Text(
+              title,
+              textAlign: TextAlign.center,
+            ),
           ),
         ),
       ),
