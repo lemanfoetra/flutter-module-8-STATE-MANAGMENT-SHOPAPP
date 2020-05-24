879564e9792ebfbaf36f486412578fdd45b280c7
Leman Fotera <lemanfoetra@gmail.com>
2020-05-24 14:57:50 +0700

    Adding Product Detail Data

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -3,20 +3,53 @@ import 'package:provider/provider.dart';
 import '../providers/product_provider.dart';
 
 class ProductDetailScreen extends StatelessWidget {
-
   static const routeName = '/product-detail';
 
   @override
   Widget build(BuildContext context) {
-
     var productId = ModalRoute.of(context).settings.arguments as String;
-    final dataProduct = Provider.of<ProductsProvider>(context).findById(productId);
-    Provider<String>(create: (ctx)=> 'Hi all',);
+    final dataProduct =
+        Provider.of<ProductsProvider>(context).findById(productId);
 
     return Scaffold(
-      appBar: AppBar( 
+      appBar: AppBar(
         title: Text(dataProduct.title),
       ),
-    ); 
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+
+            // Gambar Produk
+            Container(
+              height: 250,
+              width: double.infinity,
+              child: Image.network(
+                dataProduct.imageUrl,
+                fit: BoxFit.cover,
+              ),
+            ),
+
+            SizedBox(
+              height: 10,
+            ),
+
+            // Harga Produk
+            Text(
+              "\$${dataProduct.price}",
+              style: TextStyle(
+                color: Colors.grey,
+                fontSize: 20
+              ),
+            ),
+
+            SizedBox(height: 10),
+
+
+            // Deskripsi Produk
+            Text(dataProduct.description,)
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/widgets/chart_item.dart
+++ lib/widgets/chart_item.dart
@@ -1,15 +1,12 @@
 import 'package:flutter/material.dart';
 
 class ChartItem extends StatelessWidget {
-
   final String id;
   final double price;
   final String title;
   final double quantity;
 
-
   ChartItem({this.id, this.price, this.title, this.quantity});
-
 
   @override
   Widget build(BuildContext context) {
@@ -18,13 +15,16 @@ class ChartItem extends StatelessWidget {
         padding: EdgeInsets.symmetric(horizontal: 5, vertical: 5),
         child: ListTile(
           leading: CircleAvatar(
-            child: Padding(padding: EdgeInsets.all(5), child: FittedBox(
-              child: Text('\$ $price'),
-            )),
+            child: Padding(
+              padding: EdgeInsets.all(5),
+              child: FittedBox(
+                child: Text('\$ $price'),
+              ),
+            ),
           ),
           title: Text(title),
           trailing: Text(" $quantity X"),
-          subtitle: Text(" Total = ${ (price * quantity) }"),
+          subtitle: Text("Total = ${(price * quantity)}"),
         ),
       ),
     );
